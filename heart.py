3711a0bd79b1e932f20810f9cff125daaa1924e2
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-03-18 15:09:58 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,16 @@
+#Draw heart graphics
+
+import turtle;
+from turtle import *;
+color("red");
+begin_fill();
+pensize(3);
+left(50);
+forward(133);
+circle(50,200);
+right(140);
+circle(50,200);
+turtle.delay(50)
+forward(133);
+end_fill();
+turtle.delay(500)
